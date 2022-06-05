afae730767459a74ed077df1b48406bb8a905039
abdelhak.ajbouni <abdelhak.ajbouni@gmail.com>
2022-06-05 21:14:18 +0100

    update apis

--- src/utils/apis.ts
+++ src/utils/apis.ts
@@ -1,16 +1,28 @@
 import axios from "./axios";
 
-export const fetchAllPokemon = async (limit: number = 10, offset: number = 0) => {
+export const fetchAllPokemon = async (params: FetchAllPokemonParams) => {
+  const { offset = 10, limit = 0 } = params;
   const res = await axios.get(`/pokemon`, {
     params: {
       limit,
       offset,
     }
   });
+  return res.data.results;
+}
+
+export const fetchSinglePokemon = async (params: FetchSinglePokemonParams) => {
+  const { name } = params;
+  const res = await axios.get(`/pokemon/${name}`);
   return res.data;
 }
 
-export const fetchSinglePokemon = async (name: string) => {
-  const res = await axios.get(`/pokemon/${name}`);
-  return res.data;
+
+type FetchAllPokemonParams = {
+  limit: number
+  offset: number
+}
+
+type FetchSinglePokemonParams = {
+  name: string
 }
